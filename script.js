6f46241512bd3c7a01c06f780feb80f7ad2cc603
Islom <jolliboyevislom@gmail.com>
2022-11-14 12:24:10 +0500

    header

---
+++ script.js
@@ -0,0 +1,37 @@
+var ism = prompt("Ismingizni kiriting")
+console.log("Meni ismim "+ ism);
+
+var yosh = prompt("Sizning yoshingiz")
+console.log( "Men  "+ yosh + " yoshdaman");
+
+var misol1 =+prompt("6+6=?")
+console.log("Misol 1: " + " 6 + 6= 12 "+ " sizning javobingiz:"+  misol1);
+
+var misol2 =+prompt("10-4=?")
+console.log( "Misol 2: " + " 10 - 4 = 6 "+ " sizning javobingiz:"+  misol2);
+
+var misol3 =+prompt("2*2=?")
+console.log( "Misol 3: " + " 2 * 2 = 4 "+ " sizning javobingiz:"+  misol3);
+
+var misol4 =+prompt("6/2=?")
+console.log("Misol 4: " + " 6 / 2 = 3 "+ " sizning javobingiz:"+  misol4);
+
+var misol5 =+prompt("misloni yeching: " +"10%3=?")
+console.log("Misol 5: "+ " 10 % 3 = 1 "+ " sizning javobingiz:"+  misol5);
+
+var misol6 =+prompt("652+566=?")
+console.log("Misol 6: "+ " 652 + 566 = 1 218 "+ " sizning javobingiz:"+  misol6);
+
+var misol7 =+prompt("10*5=?")
+console.log("Misol 7: "+ " 10 * 5 = 50 "+ " sizning javobingiz:"+  misol7);
+
+var misol8 =+prompt("50/5=?")
+console.log("Misol8: "+ " 50 / 5 = 10 "+ " sizning javobingiz:"+  misol8);
+
+var misol9 =+prompt("153%5=?")
+console.log("Misol 9: "+ " 153 % 5 = 1 "+ " sizning javobingiz:"+  misol9);
+
+var misol10 =+prompt("99-1=?")
+console.log("Misol 10: "+ " 99 - 1 = 98 "+ " sizning javobingiz:"+  misol10);
+
+alert('Javobini bilish uchun consolega kiring');
